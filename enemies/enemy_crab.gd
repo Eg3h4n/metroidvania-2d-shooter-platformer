575976104fc5f5b5fda9f49cc24e120613750507
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2025-02-12 17:29:57 +0300

    added parallax background effect

--- enemies/enemy_crab.gd
+++ enemies/enemy_crab.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
 
 @export var patrol_points : Node
-
-const SPEED = 800.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED : int = 800
+@export var wait_time : int = 3
 
 enum State {Idle, Walk}
 var current_state : State
@@ -13,8 +13,10 @@ var number_of_points : int
 var point_positions : Array[Vector2]
 var current_point : Vector2
 var current_point_position : int
+var can_walk : bool
 
 func _ready() -> void:
+	timer.wait_time = wait_time
 	if patrol_points != null:
 		number_of_points = patrol_points.get_children().size()
 		for point in patrol_points.get_children():
@@ -35,28 +37,39 @@ func _physics_process(delta: float) -> void:
 	enemy_animations()
 
 func enemy_idle(delta: float) -> void:
-	if is_on_floor():
+	if is_on_floor() and !can_walk:
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
 		current_state = State.Idle
 		
 func enemy_walk(delta: float) -> void:
+	if !can_walk:
+		return
+		
 	if abs(position.x - current_point.x) > 0.5:
 		velocity.x = direction.x * SPEED * delta
 		current_state = State.Walk
+		
+		if current_point.x > position.x:
+			direction = Vector2.RIGHT
+		else:
+			direction = Vector2.LEFT
+			
 	else:
 		current_point_position = (current_point_position + 1) % number_of_points
 		
-	current_point = point_positions[current_point_position]
-	
-	if current_point.x > position.x:
-		direction = Vector2.RIGHT
-	else:
-		direction = Vector2.LEFT
+		current_point = point_positions[current_point_position]
+		
+		can_walk = false
+		timer.start()
 		
 	animated_sprite_2d.flip_h = direction.x > 0
 	
 func enemy_animations():
-	if current_state == State.Idle:
+	if current_state == State.Idle and !can_walk:
 		animated_sprite_2d.play("idle")
-	elif current_state == State.Walk:
+	elif current_state == State.Walk and can_walk:
 		animated_sprite_2d.play("walk")
+
+
+func _on_timer_timeout() -> void:
+	can_walk = true
