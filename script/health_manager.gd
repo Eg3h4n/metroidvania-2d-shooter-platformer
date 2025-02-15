ced6de5a20536b0be2521a9da7b6daace7a6245a
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2025-02-15 13:54:03 +0300

    health bar and manager added

--- script/health_manager.gd
+++ script/health_manager.gd
@@ -3,6 +3,8 @@ extends Node
 var max_health : int = 3
 var current_health : int
 
+signal on_health_change
+
 func _ready():
 	current_health = max_health
 
@@ -12,8 +14,12 @@ func decrease_health(health_amount : int):
 	if current_health < 0:
 		current_health = 0
 		
+	on_health_change.emit(current_health)
+		
 func increase_health(health_amount : int):
 	current_health += health_amount
 	
 	if current_health > max_health:
 		current_health = max_health 
+		
+	on_health_change.emit(current_health)
