82c8d426525879b715066fb6f386ba8ab679746f
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2025-02-20 15:24:40 +0300

    changed scale mode form fractional to integer

--- ui/settings_menu_screen.gd
+++ ui/settings_menu_screen.gd
@@ -3,13 +3,14 @@ extends CanvasLayer
 @onready var window_mode_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/WindowModeOptionButton
 @onready var resolution_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ResolutionOptionButton
 
-var window_modes : Dictionary = {"Fullscreen": DisplayServer.WINDOW_MODE_FULLSCREEN, 
+var window_modes : Dictionary = {"Fullscreen": DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, 
 								 "Window": DisplayServer.WINDOW_MODE_WINDOWED, 
 								 "Window Maximized": DisplayServer.WINDOW_MODE_MAXIMIZED }
 var resolutions : Dictionary = {"320x180": Vector2i(320, 180),
 								"480x270": Vector2i(480, 270),
 								"640x360": Vector2i(640, 360),
 								"854x480": Vector2i(854, 480),
+								"860x360 (Widescreen)": Vector2i(860, 360),
 								"1280x720": Vector2i(1280, 720)}
 								
 # Called when the node enters the scene tree for the first time.
