024785e121f132f72d658470ba5c9aa677fbb865
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2025-02-20 14:59:53 +0300

    added inventory system for door keys and loading screen

--- Levels/door/door.gd
+++ Levels/door/door.gd
@@ -1,6 +1,11 @@
 extends Node2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 @export var scene_to_transition: String
+@export var key_id : String
+
+var door_open : bool
 
 func _on_exit_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -10,3 +15,24 @@ func _on_exit_area_2d_body_entered(body: Node2D) -> void:
 	await get_tree().create_timer(3.0).timeout
 	print("scene transition")
 	SceneManager.transition_to_scene(scene_to_transition)
+
+
+func _on_unlock_door_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		var has_item : bool = InventoryManager.has_inventory_item(key_id)
+		
+		if !has_item:
+			return
+		
+		if !door_open:
+			animated_sprite_2d.play("open")
+			door_open = true
+			collision_shape_2d.set_deferred("disabled", true)
+
+
+func _on_unlock_door_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		if door_open:
+			animated_sprite_2d.play("close")
+			door_open = false
+			
--- script/scene_manager.gd
+++ script/scene_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SCENE_TRANSITION_SCREEN = preload("res://ui/screen_transition/scene_transition_screen.tscn")
+
 var scenes : Dictionary = {"Level1": "res://Levels/level_1.tscn",
 							"Level2": "res://Levels/level_2.tscn"}
 							
@@ -7,5 +9,8 @@ func transition_to_scene(level: String):
 	var scene_path : String = scenes.get(level)
 	
 	if scene_path != null:
-		await get_tree().create_timer(1.0).timeout
+		var scene_transition_screen_instance = SCENE_TRANSITION_SCREEN.instantiate()
+		get_tree().root.add_child(scene_transition_screen_instance)
+		await get_tree().create_timer(5.0).timeout
 		get_tree().change_scene_to_file(scene_path)
+		scene_transition_screen_instance.queue_free()
